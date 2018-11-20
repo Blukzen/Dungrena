f3c80155647d9b88994d4d2e28fed91dfbe9f4d6
Braden Roberts-Letiu <bradenrletiu@gmail.com>
2018-11-20 15:45:06 +1300

    Enemies now avoid spike traps
    ### Changed
    - [SpikeTrap] Now updates its A* penalty

--- Assets/Scripts/Objects/SpikeTrap.cs
+++ Assets/Scripts/Objects/SpikeTrap.cs
@@ -1,63 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
-public class SpikeTrap : MonoBehaviour 
+public class SpikeTrap : MonoBehaviour
 {
-	public int spikeCooldown;
-	private float timePassed;
-	public int trapDamage;
-	private new BoxCollider2D collider2D;
-	private Animator animator;
-	void Awake () 
-	{
-		animator = GetComponent<Animator>();
+    public int spikeCooldown;
+    private float timePassed;
+    public int trapDamage;
+    private new BoxCollider2D collider2D;
+    private Animator animator;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
 
-		collider2D = GetComponent<BoxCollider2D>();
-		collider2D.isTrigger = true;
-		collider2D.enabled = false;
-	}
+        collider2D = GetComponent<BoxCollider2D>();
+        collider2D.isTrigger = true;
+        collider2D.enabled = false;
+    }
 
-	private void Update() 
-	{
-		if (animator.GetCurrentAnimatorStateInfo(0).IsName("Spike"))
-			return;
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Spike"))
+            return;
 
-		if (timePassed > spikeCooldown) {
-			ActivateTrap();
-		} else {
-			timePassed += Time.deltaTime;
-		}
-	}
+        if (timePassed > spikeCooldown)
+        {
+            ActivateTrap();
+        }
+        else
+        {
+            timePassed += Time.deltaTime;
+        }
+    }
 
-	public void ActivateTrap() 
-	{
-		animator.Play("Spike");
-		timePassed = 0;
-	}
+    public void ActivateTrap()
+    {
+        animator.Play("Spike");
+        timePassed = 0;
+    }
 
-	protected void SpikesOn() 
-	{
-		collider2D.enabled = true;
-		var bounds = new Bounds(collider2D.bounds.center, collider2D.bounds.size);
-		bounds.Expand(0.6f);
-		AstarPath.active.UpdateGraphs(bounds);
-	}
+    protected void SpikesOn()
+    {
+        collider2D.enabled = true;
+        var bounds = new Bounds(collider2D.bounds.center, collider2D.bounds.size);
+        bounds.Expand(0.8f);
 
-	protected void SpikesOff() 
-	{
-		collider2D.enabled = false;
-		var bounds = new Bounds(collider2D.bounds.center, collider2D.bounds.size);
-		bounds.Expand(0.6f);
-		AstarPath.active.UpdateGraphs(bounds);
-	}
+        var guo = new GraphUpdateObject(bounds);
+        guo.addPenalty = 2000;
 
-	private void OnTriggerEnter2D(Collider2D other) 
-	{
-		var entity = other.GetComponent<AbstractEntity>();
+        AstarPath.active.UpdateGraphs(guo);
+    }
 
-		if (entity == null) return;
+    protected void SpikesOff()
+    {
+        collider2D.enabled = false;
+        var bounds = new Bounds(collider2D.bounds.center, collider2D.bounds.size);
+        bounds.Expand(0.6f);
+        var guo = new GraphUpdateObject(bounds);
+        guo.addPenalty = 2000;
 
-		entity.ApplyAttack(trapDamage);
-	}
+        AstarPath.active.UpdateGraphs(guo);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var entity = other.GetComponent<AbstractEntity>();
+
+        if (entity == null) return;
+
+        entity.ApplyAttack(trapDamage);
+    }
 }
